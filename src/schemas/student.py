78263de77186412b520bc9bf1b64e7f3c24f5388
Pokrzw <paulinab2014@interia.pl>
2022-02-08 22:33:42 +0100

    fixed edit-student function in teacher class

--- src/schemas/student.py
+++ src/schemas/student.py
@@ -2,14 +2,22 @@ import re
 class Student:
     id = 0
     def __init__(self, name:str, surname:str, classCode:str, grades:list, notes:list ):
-        self.name = name
+        self.id = Student.id
+        self.name = name.checkName(name)
         self.surname = surname
         self.classCode = classCode
         self.grades = []
         self.notes = []
+        id += 1
     def checkName(self, name):
         namePattern = '[A-Z][a-z]*'
-        if namePattern.match(name)!=None:
-            return 
+        if namePattern.match(name) is not None:
+            return name
+        raise ValueError("Given name is not correct")
+
     def checkClassCode(self, classCode):
-        pass
+        classCodePattern = '[1-8][A-G]'
+        if classCodePattern.match(classCode) is not None:
+            return classCode
+        raise ValueError("Given classCode is not correct")
+
