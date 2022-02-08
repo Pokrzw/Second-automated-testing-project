5237e11d97ce46a6abfd19ceaaa1571919a4fa9f
Pokrzw <paulinab2014@interia.pl>
2022-02-09 00:02:23 +0100

    initialized add-database tests for grade class

--- src/schemas/student.py
+++ src/schemas/student.py
@@ -1,7 +1,10 @@
 import re
+
+
 class Student:
     id = 0
-    def __init__(self, name:str, surname:str, classCode:str ):
+
+    def __init__(self, name: str, surname: str, classCode: str):
         self.id = Student.id
         self.name = self.checkName(name)
         self.surname = self.checkName(surname)
@@ -9,26 +12,28 @@ class Student:
         self.grades = []
         self.notes = []
         id += 1
+
     @staticmethod
     def checkName(name):
         namePattern = r'[A-Z][a-z]*'
-        if re.fullmatch(namePattern,name) is not None:
+        if re.fullmatch(namePattern, name) is not None:
             return name
         raise ValueError("Given name is not correct")
 
     @staticmethod
     def checkClassCode(classCode):
         classCodePattern = r'[1-8][A-G]'
-        if re.fullmatch(classCodePattern,classCode) is not None:
+        if re.fullmatch(classCodePattern, classCode) is not None:
             return classCode
         raise ValueError("Given classCode is not correct")
 
     @property
-    def grades(self) -> str:
+    def grades(self):
         return self.grades
 
     @property
-    def notes(self) -> str:
+    def notes(self):
         return self.notes
 
-newStudent = Student("Jan","Kowalski","3A")
+
+newStudent = Student("Jan", "Kowalski", "3A")
