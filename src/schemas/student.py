07ea49a9f90c178c83c20c0965807134bff1b4f3
Pokrzw <paulinab2014@interia.pl>
2022-02-08 22:23:39 +0100

    added check-student method for grade class

--- src/schemas/student.py
+++ src/schemas/student.py
@@ -1,3 +1,4 @@
+import re
 class Student:
     id = 0
     def __init__(self, name:str, surname:str, classCode:str, grades:list, notes:list ):
@@ -7,3 +8,9 @@ class Student:
         self.grades = []
         self.notes = []
     def checkName(self, name):
+        pattern = re.compile('[A-Z]{1}[a-z]')
+        pattern.match(string)
+
+        nameRegex =
+    def checkClassCode(self, classCode):
+        pass
