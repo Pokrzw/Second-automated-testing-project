e24413b4a3c5199c828ad78337a36852d89c622f
Pokrzw <paulinab2014@interia.pl>
2022-02-09 18:17:15 +0100

    initialized edit-grade function for grade class

--- tests/test_grades.py
+++ tests/test_grades.py
@@ -17,3 +17,25 @@ class TestAddGrade(unittest.TestCase):
     def test_add_grade_fail(self, mock_add):
         mock_add.return_value = -1
         assert_that(self.app.add_grade(10, 10, 10, 10, 4)).is_equal_to(False)
+
+class TestEditGrade(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = App()
+    @patch("src.database.Database.check_if_grade_exists")
+    def test_edit_grade_fail_doesnt_exist(self, mock_check):
+        mock_check.return_value = None
+        assert_that(self.app.edit_grade(10,1,1,1,3)).is_equal_to("This grade does not exist")
+
+    @patch("src.database.Database.edit_grade")
+    @patch("src.database.Database.check_if_grade_exists")
+    def test_edit_grade_fail(self, mock_check, mock_edit):
+        mock_check.return_value = "grade instance"
+        mock_edit.return_value = -1
+        assert_that(self.app.edit_grade(1,1,1,1,3)).is_equal_to(False)
+
+    @patch("src.database.Database.edit_grade")
+    @patch("src.database.Database.check_if_grade_exists")
+    def test_edit_grade_fail(self, mock_check, mock_edit):
+        mock_check.return_value = "grade instance"
+        mock_edit.return_value = 1
+        assert_that(self.app.edit_grade(1,1,1,1,3)).is_equal_to(True)
