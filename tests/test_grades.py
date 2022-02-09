c3c50e22b60004eac62fb0d5568e66b849d0fab8
Pokrzw <paulinab2014@interia.pl>
2022-02-09 18:07:11 +0100

    improved edit-student variable for grade class

---
+++ tests/test_grades.py
@@ -0,0 +1,14 @@
+from unittest.mock import Mock, patch
+import unittest
+from src.app import App
+from src.database import Database
+from src.schemas.grades import Grade
+from assertpy import assert_that
+
+class TestAddGrade(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = App()
+    @patch("src.database.Database.add_grade")
+    def test_add_grade_success(self, mock_add):
+        mock_add.return_value = 1
+        assert_that(self.app.add_grade(1,1,1,1,4)).is_equal_to(True)
