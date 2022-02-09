4621e5144121c7188239942ea0ba68a7d892b061
Pokrzw <paulinab2014@interia.pl>
2022-02-09 16:21:31 +0100

    added remove-grade array in teacher class

---
+++ src/schemas/grades.py
@@ -0,0 +1,7 @@
+class Grade:
+    def __init__(self, grade_id, student_id, teacher_id, subject_id, value):
+        self.grade_id = grade_id
+        self.student_id = student_id
+        self.teacher_id = teacher_id
+        self.subject_id = subject_id
+        self.value = value
